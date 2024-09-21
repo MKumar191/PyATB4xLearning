cdf24c9c1419284b42d84c2622aed96a07ed0041
MKumar191 <mayankkumar1103@gmail.com>
2024-09-21 22:19:19 +0530

    Lab003 Added

---
+++ src/ex_10082024/Lab003.py
@@ -0,0 +1,5 @@
+# This is comment
+print("Hello World!")
+print("Hello", "World", "Amit", "Pramod", 123, 3.14, True)
+print("Hello", "World", "Amit", "Pramod", 123, 3.14, True, sep="-", end="\n")
+print("Mayank")
