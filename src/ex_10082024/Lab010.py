240b8d9fc839a1937ded0e202cc1eefd03e3d57e
MKumar191 <mayankkumar1103@gmail.com>
2024-09-22 16:38:35 +0530

    Lab010 added

---
+++ src/ex_10082024/Lab010.py
@@ -0,0 +1,7 @@
+#Dynamically typed
+#At the run time, data of the variable can be changed, and you don't need to mention the data type
+age = 45
+print(type(age))
+age = "Mayank"
+print(age)
+print(type(age))
