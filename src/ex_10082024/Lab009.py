e7203adde244bf2eb62ae157081659481d7029f6
MKumar191 <mayankkumar1103@gmail.com>
2024-09-22 15:45:58 +0530

    Lab009 added

---
+++ src/ex_10082024/Lab009.py
@@ -0,0 +1,21 @@
+age = 65
+#age - variable - identifier
+#123 = 34 -> Not possible - A-Z, a-z
+
+abc = 78
+#123abc = 90(Not possible)
+_pramod = "Mayank"
+_abc = 23
+#$123 = 67(Not possible)
+
+pi = 3.14
+name = "Mayank"
+isMale = True
+
+print(type(name))
+print(type(pi))
+print(type(isMale))
+
+complex_number = 2 + 3j
+print(complex_number.real)
+print(complex_number.imag)
