b6074a5346e43a2bbd6eca652ff6c03bb9f0ea14
MKumar191 <mayankkumar1103@gmail.com>
2024-09-23 19:06:38 +0530

    Lab016 added

---
+++ src/ex_13082024/Lab016.py
@@ -0,0 +1,13 @@
+age = 65 #int
+print(age)
+#age -> variable Name -> Identifier
+# = -> Assignment operator (right to left)
+# 65 -> variable value -> literal
+age = 95
+print(95)
+# What is the range of the integer
+a = 100 # Range for the int, float is very large - Python Interpreter - automatically until it has memory
+
+gst = 17.56
+print(type(gst))
+print(gst)
