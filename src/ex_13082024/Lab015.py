3f185a01f52a106bdd7f4723bff7e7b4c3e39dc0
MKumar191 <mayankkumar1103@gmail.com>
2024-09-23 11:07:32 +0530

    Lab014 added

---
+++ src/ex_13082024/Lab015.py
@@ -0,0 +1,21 @@
+# int -> whole number -> -1 , 2 , 100 , 19999 , 99999
+# float -> decimal numbers -> 3.14, 41.22
+# Boolean -> True of False -> isMale?
+# String -> str - Bunch of chars -> Mayank , Pyt4x , @$@$
+
+age = 34
+print(type(age))
+
+name = "Mayank"
+name2 = 'Kumar'
+print(type(name))
+print(name2)
+
+"""
+
+This is Multiple Line Comment 
+Here the Code you write will not be executed
+Why this is used?
+
+Use for Notes or Important information if u want to give
+"""
