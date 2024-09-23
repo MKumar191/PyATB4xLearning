34b1a0ff9384eea07a79c2f750bec653c7aaf2d2
MKumar191 <mayankkumar1103@gmail.com>
2024-09-23 20:06:32 +0530

    Lab018 added

---
+++ src/ex_13082024/Lab018.py
@@ -0,0 +1,38 @@
+age = 50
+#Identifier (Variable name) = literal (variable value)
+
+"""
+Identifier - Rules
+They start with a letter(A-Z or a-z) 
+and can start with underscore(_) followed by zero or more letter.
+Python is case sensitive
+
+"""
+
+a = 10
+A = 25
+# 123 = 34
+print(a)
+print(A)
+_ = 10
+print(_)
+_mayank = 25
+#$1 = 10
+
+pi = 3.14
+pi = pi + 1
+print(pi)
+
+is_married = False
+is_Male = True
+are_u_in_usa = False
+print(are_u_in_usa)
+
+#Complex
+Complex_Number = 2 + 3j
+print(Complex_Number.real)
+print(Complex_Number.imag)
+
+# j -> root of 1
+
+
